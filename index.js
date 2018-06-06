0888fcd54e0a2c297d068d33c665d95f719a4e94
Nicholas <nicholas@nicholastrobiano.com>
2018-06-06 15:56:30 -0400

    removed upfront tag

--- index.js
+++ index.js
@@ -71,33 +71,7 @@ app.post('/quote', (req, res) => {
             res.send('New Quote Created');
 
         };
-        request.post({
-            auth,
-            body: { draft_order },
-            json: true,
-            url: `https://${SHOP_URL}/admin/draft_orders/#{draft_order_id}/send_invoice.json`
-        }, (error, response, body) => {
-
-            if (draft_order.tag = "upfront") {
-                request.post({
-                    auth,
-                    body: {
-                        draft_order_invoice: {
-                            to: "nicholas@nicholastrobiano.com",
-                            from: "info@nicholastrobiano.com",
-                            bcc: [
-                            ],
-                            subject: "Testing automatic upfront tag",
-                            custom_message: "Testing message for custom upfront tag"
-                        },
-                    
-                    },
-                    json: true,
-                    url: `https://${SHOP_URL}/admin/draft_orders/#{draft_order_id}/send_invoice.json`
-                });
-
-            }
-        });
+        
 
     });
 
